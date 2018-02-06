386f38d041ba91850010b0e6e7ba7cfea606cb89
Jaleeljs <35237438+Jaleeljs@users.noreply.github.com>
2018-02-05 23:00:13 -1000

    Completed coding challenge 1

---
+++ ProjectOneJaleel.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//Variable Coding Challenge #1
+
+var firststring = "hey"
+
+var secondstring = " wassup"
+
+var thirdstring = firststring + secondstring
+
+print(thirdstring)
+
+
+//Variable Coding Challenge #2
+
+
+
